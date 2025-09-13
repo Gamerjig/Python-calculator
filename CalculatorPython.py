d41032aa8cc2eca4a7368f2086e818b828fe0c2d
ScpWIZ <131114469+Gamerjig@users.noreply.github.com>
2025-09-12 20:25:10 -0700

    Update CalculatorPython.py
    Learned how to make it loop

--- CalculatorPython.py
+++ CalculatorPython.py
@@ -1,34 +1,48 @@
-print("Calculator made by Gamerjig!")
+import os
 
-chose = str(input("Enter what sign to use {+, -, /, *}: "))
-amount = int(input("Enter how many numbers to " + chose + ": "))
+def clear():
+    os.system("cls")
 
+while True:
+    clear()
+    print("Calculator made by Gamerjig!")
 
-a = 0
-numbers = []
+    chose = str(input("Enter what sign to use {+, -, /, *}: "))
 
-for i in range(amount):
-    numbers.append(int(input("Enter num to " + chose + ": ")))
+    amount = int(input("Enter how many numbers to " + chose + ": "))
 
+    a = 0
+    numbers = []
 
+    for i in range(amount):
+        numbers.append(int(input("Enter num to " + chose + ": ")))
 
-if chose == "+":
-    a = sum(numbers)
-elif chose == "-":
-    a = numbers[0]
-    for num in numbers[1:]:
-        a -= num
-elif chose == "*":
-    a = 1
-    for num in numbers:
-        a *= num
-elif chose == "/":
-    a = numbers[0]
-    for num in numbers[1:]:
-        a /= num
-else:
-    print("Warning you must choose from +, -, /, * ")
+    if chose == "+":
+        a = sum(numbers)
+    elif chose == "-":
+        a = numbers[0]
+        for num in numbers[1:]:
+            a -= num
+    elif chose == "*":
+        a = 1
+        for num in numbers:
+            a *= num
+    elif chose == "/":
+        a = numbers[0]
+        for num in numbers[1:]:
+            a /= num
+    else:
+        print("Warning you must choose from +, -, /, * ")
 
-a = str(a)
+    print("the total number is: "+ str(a))
 
-print("the total number is: "+ a)
+    print("Use again?")
+    print("1 - Yes")
+    print("2 - No")
+    again = str(input("Enter your chose: "))
+    if again == "1":
+        print("Restarting..")
+    elif again == "2":
+        break
+    else:
+        print("Must be 1 or 2")
